32b195d3312ad5f66105e985464980c345655450
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-28 14:37:08 +0530

    Add files via upload

---
+++ Aaverageof5Num.java
@@ -0,0 +1,10 @@
+class Averageof5Num{
+public static void main(String[]args){
+  int a=3;
+  int b=5;
+  int c=7;
+  int d=9;
+  int e=1;
+ System.out.println("average of 5 num is:" +(a+b+c+d+e)/5);
+ }
+}
