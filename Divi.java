683a600d90ed02febf6b7dcb6263291b0e129b6c
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-28 14:33:28 +0530

    Add files via upload

---
+++ Divi.java
@@ -0,0 +1,13 @@
+class DivisibleBy3or7{
+ public static void main(String[]args){
+ 
+ int x=21;
+
+  if(x%3==0 && x%7==0)
+   
+     System.out.println("yes");  
+  
+  else
+     System.out.println("no");
+ }
+}
