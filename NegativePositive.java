e3693d48c6d6c51aeae34fb441b23dd77f34a499
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:45:20 +0530

    Add files via upload

---
+++ NegativePositive.java
@@ -0,0 +1,17 @@
+class NegativePositive
+ {
+   public static void main(String[]args)
+    {
+      int a=-5;
+      
+      if(a>=0)
+       
+           System.out.println(a+"=num is positive");
+      
+      else 
+             
+          System.out.println(a+"=num is negative");
+      
+     
+     }
+ }    
