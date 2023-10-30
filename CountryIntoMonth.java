9f5b7f70e4886a536d54571d717920391e649545
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:52:43 +0530

    Add files via upload

---
+++ CountryIntoMonth.java
@@ -0,0 +1,12 @@
+class CountryIntoMonth
+ {
+   public static void main(String[]args)
+     {
+ 
+        int age = 6;
+
+        int totalMonth = age*12;
+
+        System.out.println("Month=" +totalMonth);
+     }
+   }
