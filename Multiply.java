ae851f94dd02ffadfe53f1a0dfad4336398b1fae
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:46:25 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+class Multiply
+ {
+   public static void main(String[]args)
+    {
+       int  a= 5;
+       int  b= 25;
+    
+     System.out.println("Multiple of two num:" +(a*b));
+    }
+  }
