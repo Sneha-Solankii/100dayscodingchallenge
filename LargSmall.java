dc6cd68535caa7ea091520884d940ec39d834257
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:50:33 +0530

    Add files via upload

---
+++ LargSmall.java
@@ -0,0 +1,34 @@
+class LargSmall
+ {
+   public static void main(String[]args)
+    {
+      int a= 14;
+      int b= 11;
+      int c= 20;
+  
+         if(a>b && a>c)
+                System.out.println(a+" no is Largest");
+        
+         
+         else if(b>a && b>c)
+                System.out.println(b+" no is Largest");
+        
+           
+         else 
+                System.out.println(c+" no is Largest");
+          
+       
+        if(a<b && a<c)
+                System.out.println(a+" no is Smallest");
+        
+          
+        else if (b<a && b<c)
+                System.out.println(b+" no is Smallest");
+         
+            
+        else 
+                System.out.println(c+" no is Smallest");
+       }
+  }
+        
+         
