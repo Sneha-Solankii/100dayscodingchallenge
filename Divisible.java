5b61cde67ad508ad3dd2addead16e71a079f7c65
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:47:01 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,17 @@
+class Divisible
+ {
+   public static void main(String[]args)
+    {
+      int num = 25;
+      
+      if(num%3==0 && num%7==0)
+       
+           System.out.println(num+"Number is divisible");
+      
+      else 
+             
+          System.out.println(num+"Number is not divisible");
+      
+     
+     }
+ }    
