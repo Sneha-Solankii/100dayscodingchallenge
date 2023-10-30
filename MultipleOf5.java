b579d190157dbe0528b0470be8a7cdbb64440b7c
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:48:21 +0530

    Add files via upload

---
+++ MultipleOf5.java
@@ -0,0 +1,15 @@
+class MultipleOf5
+ {
+   public  static void main(String[]args)
+    {
+      int num = 12;
+      
+      if(num%5==0)
+       
+        System.out.println(num+" is Multiple of 5");
+   
+      else
+        
+        System.out.println(num+" is not Multiple of 5");
+    }
+ }
