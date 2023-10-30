5fd24cf5128882ffcd0548b4d9eb5ebeff7762ed
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:50:51 +0530

    Add files via upload

---
+++ CheckNum.java
@@ -0,0 +1,16 @@
+class CheckNum
+ {
+   public static void main(String[]args)
+     {
+      
+       int a = 45;
+
+         if(a>=21 && a<=575)
+
+            System.out.println("Yes number is valid");
+ 
+         else
+ 
+            System.out.println("No number is not valid");
+      }
+ }
