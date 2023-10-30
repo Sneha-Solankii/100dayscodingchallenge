6307cf6335627a1c735acc84870f61ef7fa5dc81
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-10-30 16:51:41 +0530

    Add files via upload

---
+++ CheckAlphabet.java
@@ -0,0 +1,15 @@
+class CheckAlphabet
+  {
+    public static void main(String[]args)
+     {
+       int value = '&';
+     
+       if(value>='a' && value<='z' || value>='A' && value<='Z')
+
+           System.out.println("Alphabet");   
+
+       else
+  
+           System.out.println("not Alphabet");
+     }
+   }
