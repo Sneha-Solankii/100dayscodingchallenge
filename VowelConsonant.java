3c789713a09c5c5ef245c66e8429d2ce908208ee
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-01 18:54:43 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,19 @@
+
+
+
+class VowelConsonant
+ {
+   public static void main(String... args)
+    {
+      
+     char ch='e';
+ 
+  if(ch=='a'||ch=='e'||ch=='i'||ch=='0'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+   
+     System.out.println("It is Vowel");
+
+  else
+
+     System.out.println("It is Consonant");
+ }
+}
