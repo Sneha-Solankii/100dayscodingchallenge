ebc87d7eff6aef9f5dae429a94c3e799c889a0b7
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-02 22:15:09 +0530

    Add files via upload

---
+++ Absolute.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Absolute
+ {
+   public static void main(String... args)
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the number");
+      int num=sc.nextInt();
+
+   if(num>=0)
+     
+      System.out.println(num);
+
+   else 
+
+       System.out.println(-num);
+
+   }
+}
+
+      
