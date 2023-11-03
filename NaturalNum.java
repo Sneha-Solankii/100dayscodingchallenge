c70514c5340ce9aad022f66c18f0749716d2754f
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-03 20:52:43 +0530

    Add files via upload

---
+++ NaturalNum.java
@@ -0,0 +1,17 @@
+class NaturalNum
+ {
+  public static void main(String[]args)
+   {
+     
+    int i=1;
+   
+    while(i<=10)
+
+      {
+
+         System.out.println("Natural number is:" +i);
+         i++;
+
+      }
+    }
+  }
