669c27ed0f63e5289cba81d55981c8223d6ec952
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-03 20:49:21 +0530

    Add files via upload

---
+++ EvenNum.java
@@ -0,0 +1,19 @@
+class EvenNum
+ {
+   public static void main(String[]args)
+    {
+     
+      int i=0;
+
+      while( i<=10)
+          
+        if(i%2==0)
+          {
+            System.out.println("Even number is :" +i);
+          }          
+        i++;
+
+
+        
+      }
+   }
