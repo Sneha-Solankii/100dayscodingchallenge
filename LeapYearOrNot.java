9c05accd2f8230f433aa40b18c94d4abdb5b842a
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-03 20:48:34 +0530

    Add files via upload

---
+++ LeapYearOrNot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+class LeapYearOrNot
+ {
+   public static void main(String[]args)
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enetr the year");
+      int year=sc.nextInt();
+
+      if(year%4 == 0 && year%400 == 0)
+
+        System.out.println("year is leap year=" +year);
+ 
+      else
+
+        System.out.println("year is not leap year=" +year);
+
+    }
+  }
