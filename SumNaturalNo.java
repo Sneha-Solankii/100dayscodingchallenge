90a3bbd0480aa1e746f35ab28b8261e3aaabf861
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-06 18:32:01 +0530

    Add files via upload

---
+++ SumNaturalNo.java
@@ -0,0 +1,14 @@
+class SumNaturalNo
+ {
+   public static void main(String[]args)
+     {
+       int i=0,sum=0;
+   
+        while(i<=5)
+         {
+           sum = sum+i;
+           i++;
+         }
+       System.out.println("Sum of natural no.:" +sum);
+     }
+  }
---
+++ TableOf5.java
@@ -0,0 +1,15 @@
+class TableOf5
+ {
+   public static void main(String[]args)
+     {
+       int i=1,n=5,table;
+          
+         while(i<=10)
+          {
+            table=n*i;
+            System.out.println(n+ " x " +i+ " = " +table);
+          
+            i++;
+          }
+      }
+  }
