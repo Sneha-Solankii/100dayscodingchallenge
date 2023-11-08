af86976efc3190491a24b62b680fecfd73ed94e3
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-08 20:14:07 +0530

    Add files via upload

---
+++ Square_1to7.java
@@ -0,0 +1,15 @@
+class Square_1to7
+   {
+      public static void main(String[]args)
+        {
+          
+          int square,i;
+ 
+          for(i=1; i<=7; i++)
+            { 
+              square = i*i;
+
+              System.out.println("Square of "+i+"=" +square);
+            }
+         }
+   }
