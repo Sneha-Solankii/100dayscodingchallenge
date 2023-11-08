90a4876e7fd736957c7f55ed9ceb232d200fb785
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-08 20:16:44 +0530

    Add files via upload

---
+++ PrintAlphabate.java
@@ -0,0 +1,14 @@
+
+
+class PrintAlphabate
+ {
+   public static void main(String[]args)
+    {
+      
+       
+     for(char ch='a'; ch<='z'; ch++)
+        { 
+           System.out.println(ch);
+        }
+     }
+  }
