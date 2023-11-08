85cdea48def7258aa819714365d8988ea307a1b0
Sneha-Solankii <146639927+Sneha-Solankii@users.noreply.github.com>
2023-11-08 20:08:54 +0530

    Add files via upload

---
+++ ProductNum.java
@@ -0,0 +1,14 @@
+class ProductNum
+  {
+     public static void main(String[]args)
+       {
+         int i, product=1;
+
+         for(i=1; i<=10; i++)
+           { 
+             product = product * i;
+            
+             System.out.println("Product number: " +product);
+           }
+        }
+   }
